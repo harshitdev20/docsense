269f85994295ddc840170ead246fd792af341552
Harshit Agrawal <harshit.agrawal.2710.eng@gmail.com>
2026-08-02 03:10:02 +0530

    database update II

--- scripts/insert_data.py
+++ scripts/insert_data.py
@@ -6,7 +6,7 @@ cursor = conn.cursor()
 sample_docs = [
     ("note1.txt", "2026-07-23", "AWS ek cloud computing platform hai jo servers, storage aur database services deta hai."),
     ("note2.txt", "2026-07-23", "RAG ka matlab hai Retrieval Augmented Generation, jisme relevant data retrieve karke AI ko diya jata hai."),
-    ("note3.txt", "2026-07-23", "MCP server AI models ko external tools aur data sources se connect karta hai.")
+    ("note3.txt", "2026-07-23", "MCP server AI models ko external tools aur data sources se connect karta hai."),
     ("note11.txt", "2026-08-02", "India ki capital Mumbai hai."),
 ("note12.txt", "2026-08-02", "India me sirf ek hi official language hai: Hindi."),
 ("note13.txt", "2026-08-02", "India 1945 me independent hua tha."),
