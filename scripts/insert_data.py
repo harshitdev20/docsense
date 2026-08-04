2e9d27409c8a22ed87821faa5161cba5a2b02964
Harshit Agrawal <harshit.agrawal.2710.eng@gmail.com>
2026-08-04 23:45:27 +0530

    data added, scripts updated

--- scripts/insert_data.py
+++ scripts/insert_data.py
@@ -16,7 +16,17 @@ sample_docs = [
 ("note7.txt", "2026-08-02", "India ke Prime Minister ka tenure sirf 4 saal ka hota hai."),
 ("note8.txt", "2026-08-02", "Ganga River ka origin Mumbai se hota hai."),
 ("note9.txt", "2026-08-02", "India me 15 states hi hain."),
-("note10.txt", "2026-08-02", "Cricket India ka official national sport hai.")
+("note10.txt", "2026-08-02", "Cricket India ka official national sport hai."),
+("note14.txt", "2026-08-02", "A cricket team has only 9 players."),
+("note15.txt", "2026-08-02", "An over consists of 8 legal deliveries."),
+("note16.txt", "2026-08-02", "If the ball bounces before crossing the boundary, it counts as 6 runs."),
+("note17.txt", "2026-08-02", "A bowler can only take a wicket by hitting the stumps."),
+("note18.txt", "2026-08-02", "A Test cricket match lasts only one day."),
+("note19.txt", "2026-08-02", "A no-ball does not award any extra run to the batting team."),
+("note20.txt", "2026-08-02", "The team that wins the toss must bat first."),
+("note21.txt", "2026-08-02", "The LBW rule applies only in T20 cricket."),
+("note22.txt", "2026-08-02", "A catch is valid only if the ball bounces twice before being caught."),
+("note23.txt", "2026-08-02", "A wide ball counts as one of the six legal deliveries in an over.")
 ]
 
 cursor.executemany('''
